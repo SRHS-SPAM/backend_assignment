5744185e9808f2f1fac6eca05cf2c0b3eab0aa71
3boku <hyunseojung@clika.io>
2024-09-26 16:33:20 +0900

    feat: DB 쿼리 작성

--- 1/hyunseo/repositories/shop.go
+++ 1/hyunseo/repositories/shop.go
@@ -9,17 +9,37 @@ type ShopService struct {
 	DB *gorm.DB
 }
 
-func (s *ShopService) SelectALL() (data []*types.Shop, err error) {
+func (s *ShopService) SelectALL() (data []*types.ShopDAO, err error) {
 	err = s.DB.Find(&data).Error
 	return data, err
 }
 
-func (s *ShopService) SelectByName(name string) (data *types.Shop, err error) {
+func (s *ShopService) SelectByName(name string) (data *types.ShopDAO, err error) {
 	err = s.DB.First(&data, "name =?", name).Error
 	return data, err
 }
 
-func (s *ShopService) Create(shop *types) (data *types.Shop, err error) {
-	err = s.DB.Create(&shop).Error
+func (s *ShopService) Create(shop *types.ShopDAO) (data *types.ShopDAO, err error) {
+	input := types.ShopDTO{
+		Name:      shop.Name,
+		OwnerName: shop.OwnerName,
+		Category:  shop.Category,
+	}
+	err = s.DB.Create(&input).Error
 	return data, err
+}
+
+func (s *ShopService) Update(id string, shop *types.ShopDAO) (data *types.ShopDAO, err error) {
+	input := types.ShopDTO{
+		Name:      shop.Name,
+		OwnerName: shop.OwnerName,
+		Category:  shop.Category,
+	}
+	err = s.DB.Model(&types.ShopDTO{}).Where("id =?", id).Save(input).Error
+	return data, err
+}
+
+func (s *ShopService) Delete(id string) (err error) {
+	err = s.DB.Delete(&types.ShopDTO{}, id).Error
+	return err
 }
