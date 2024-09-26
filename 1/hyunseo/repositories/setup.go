6120f7ef01f5e999703635914740dc7d5b5e06fc
3boku <hyunseojung@clika.io>
2024-09-26 15:53:53 +0900

    feat: 디비 연결

--- 1/hyunseo/repositories/setup.go
+++ 1/hyunseo/repositories/setup.go
@@ -1,7 +1,12 @@
 package repositories
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
 
-func SetupDB() {
-	db, err := gorm.Open()
+func NewPostgres(dsn string) (db *gorm.DB, err error) {
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	return db, err
 }
